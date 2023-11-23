ef4ca5a30acb19b6a4c5a780d879040cc0437980
alessandroordetti <alessandro.ordetti@gmail.com>
2023-11-23 17:00:41 +0100

    WIP

--- controllers/admin/index.php
+++ controllers/admin/index.php
@@ -6,8 +6,10 @@ use Core\Database;
 $db = App::resolve(Database::class);
 
 $latest5Users = $db->query("SELECT users.email FROM users LIMIT 5 ")->get();
+$latest5Posts = $db->query("SELECT notes.body FROM notes LIMIT 4")->get();
 
 
 view('admin/index-view.php', [
-    'latestUsers' => $latest5Users
+    'latestUsers' => $latest5Users,
+    'latest5Posts' => $latest5Posts
 ]);
--- views/admin/index-view.php
+++ views/admin/index-view.php
@@ -23,7 +23,7 @@
                                     <div class="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
                                 </div>
                                 <div class="flex flex-col items-center text-center text-slate-200 text-xl font-bold">
-                                    <h1 class="mb-5">Latest users</h1>
+                                    <h1 class="mb-5">Latest logged users</h1>
                                 </div>
 
                                 <div>
@@ -52,15 +52,15 @@
                                         <img class="inline-flex" src="https://cruip-tutorials.vercel.app/spotlight-effect/card-02.png" width="200" height="200" alt="Card 02" />
                                     </div>
                                     <!-- Text -->
-                                    <div class="grow mb-5">
-                                        <h2 class="text-xl text-slate-200 font-bold mb-1">Amazing Integration</h2>
-                                        <p class="text-sm text-slate-300">Quickly apply filters to refine your issues lists and create custom views.</p>
+                                    <div class="grow h-100 mb-5 flex flex-col-reverse">
+                                        <p class="text-sm text-slate-300">Quickly handle users' profiles</p>
+                                        <h2 class="text-xl text-pink-600 font-bold mb-1">Handle users</h2>
                                     </div>
                                     <a class="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150" href="#0">
                                         <svg class="fill-slate-500 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="14">
                                             <path d="M12.82 8.116A.5.5 0 0 0 12 8.5V10h-.185a3 3 0 0 1-2.258-1.025l-.4-.457-1.328 1.519.223.255A5 5 0 0 0 11.815 12H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM12.82.116A.5.5 0 0 0 12 .5V2h-.185a5 5 0 0 0-3.763 1.708L3.443 8.975A3 3 0 0 1 1.185 10H1a1 1 0 1 0 0 2h.185a5 5 0 0 0 3.763-1.708l4.609-5.267A3 3 0 0 1 11.815 4H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM1 4h.185a3 3 0 0 1 2.258 1.025l.4.457 1.328-1.52-.223-.254A5 5 0 0 0 1.185 2H1a1 1 0 0 0 0 2Z" />
                                         </svg>
-                                        <span>Connect</span>
+                                        <span>Action</span>
                                     </a>
                                 </div>
                             </div>
@@ -74,21 +74,22 @@
                                     <div class="absolute inset-0 translate-z-0 bg-slate-800 rounded-full blur-[80px]"></div>
                                 </div>
                                 <div class="flex flex-col h-full items-center text-center">
-                                    <!-- Image -->
-                                    <div class="relative inline-flex">
-                                        <div class="w-[40%] h-[40%] absolute inset-0 m-auto -translate-y-[10%] blur-3xl -z-10 rounded-full bg-indigo-600" aria-hidden="true"></div>
-                                        <img class="inline-flex" src="https://cruip-tutorials.vercel.app/spotlight-effect/card-03.png" width="200" height="200" alt="Card 03" />
-                                    </div>
+                                    <ul>
+                                        <?php foreach ($latest5Posts as $post) { ?>
+                                            <li class="text-sm text-slate-300 mb-5 font-bold"><?php echo $post['body'] ?></li>
+                                        <?php } ?>
+                                    </ul>
+                                    
                                     <!-- Text -->
                                     <div class="grow mb-5">
-                                        <h2 class="text-xl text-slate-200 font-bold mb-1">Amazing Integration</h2>
-                                        <p class="text-sm text-slate-300">Quickly apply filters to refine your issues lists and create custom views.</p>
+                                        <h2 class="text-xl text-pink-600 font-bold mb-1">Handle notes</h2>
+                                        <p class="text-sm text-slate-300">Quickly handle users' notes</p>
                                     </div>
                                     <a class="inline-flex justify-center items-center whitespace-nowrap rounded-lg bg-slate-800 hover:bg-slate-900 border border-slate-700 px-3 py-1.5 text-sm font-medium text-slate-300 focus-visible:outline-none focus-visible:ring focus-visible:ring-indigo-300 dark:focus-visible:ring-slate-600 transition-colors duration-150" href="#0">
                                         <svg class="fill-slate-500 mr-2" xmlns="http://www.w3.org/2000/svg" width="16" height="14">
                                             <path d="M12.82 8.116A.5.5 0 0 0 12 8.5V10h-.185a3 3 0 0 1-2.258-1.025l-.4-.457-1.328 1.519.223.255A5 5 0 0 0 11.815 12H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM12.82.116A.5.5 0 0 0 12 .5V2h-.185a5 5 0 0 0-3.763 1.708L3.443 8.975A3 3 0 0 1 1.185 10H1a1 1 0 1 0 0 2h.185a5 5 0 0 0 3.763-1.708l4.609-5.267A3 3 0 0 1 11.815 4H12v1.5a.5.5 0 0 0 .82.384l3-2.5a.5.5 0 0 0 0-.768l-3-2.5ZM1 4h.185a3 3 0 0 1 2.258 1.025l.4.457 1.328-1.52-.223-.254A5 5 0 0 0 1.185 2H1a1 1 0 0 0 0 2Z" />
                                         </svg>
-                                        <span>Connect</span>
+                                        <span>Action</span>
                                     </a>
                                 </div>
                             </div>
